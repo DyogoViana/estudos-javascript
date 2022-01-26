0f54aa87ca6d12ed10c3dce8e75956c60477dc68
DyogoViana <dyogo.vianaa@gmail.com>
2022-01-26 12:14:06 -0300

    Programa de Fuso Horário :: Cap 3 . Condições

--- source/pages/cap03 - Condições/3.4/js/ex3_4.js
+++ source/pages/cap03 - Condições/3.4/js/ex3_4.js
@@ -5,5 +5,30 @@
 	de +5 horas (horário de verão da França).
 
 	- Elaborar um programa que leia a hora no Brasil e informe a hora na França.
-
 */
+
+function fusoHorario() {
+	let inHoraBrasil = document.getElementById("inHoraBrasil");
+	let outResultadoFuso = document.getElementById("outResultadoFuso");
+
+	horaBrasil = Number(inHoraBrasil.value);
+
+	if (inHoraBrasil.value == "" || isNaN(horaBrasil)) {
+		alert("Por favor, informe a hora do Brasil corretamente.");
+		inHoraBrasil.focus();
+		return;
+	}
+
+	let horaFranca = horaBrasil + 4;
+
+	// Se passar das 24h na frança...
+	if (horaFranca > 24) {
+		horaFranca = horaFranca - 24;
+	}
+
+	outResultadoFuso.textContent = "Hora na França: " + horaFranca.toFixed(2);
+}
+
+const exibirHoraFranca = document.getElementById("exibirHoraFranca");
+exibirHoraFranca.addEventListener("click", fusoHorario);
+
