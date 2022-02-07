0c8bb3c38e55b9bb896e6c2822f5663f246c0ba6
DyogoViana <dyogo.vianaa@gmail.com>
2022-02-07 15:41:40 -0300

    AJUSTE :: Programa Caixa Eletrônico -- exercício 3.6 -- Condições.

--- source/pages/cap03 - Condições/3.6/js/index.js
+++ source/pages/cap03 - Condições/3.6/js/index.js
@@ -25,51 +25,57 @@ function exibirValorSaques() {
 	let outNotas50 = document.getElementById("outNotas50");
 	let outNotas10 = document.getElementById("outNotas10");
 
-	let valorSaque = Number(inValorSaque.value);
-
 	// Limpa mensagens (caso, segunda execução).
 	outNotas100.textContent = "";
 	outNotas50.textContent = "";
 	outNotas10.textContent = "";
 
+	let valorSaque = Number(inValorSaque.value);
+
 	if (valorSaque == 0 || isNaN(valorSaque)) {
-		alert("Por favor, preencha um valor válido.")
+		alert("Favor informar um valor válido.");
 		inValorSaque.focus();
 		return;
 	}
 
-	// Calcula notas de 100, 50 e 10.
+	// Verificar se o saque não é múltiplo de dez.
 	if (valorSaque % 10 != 0) {
-		alert("Valor inválido para as notas disponíveis (100, 50 e 10).")
+		alert("Valor inválido para notas disponíveis (R$ 100, 50 e 10).")
 		inValorSaque.focus();
 		return;
 	}
 
 	// Calcula notas de 100, 50 e 10.
 	const cedula_100 = Math.floor(valorSaque / 100);
-	const resto = valorSaque % 100;
+	let resto = valorSaque % 100;
 
 	const cedula_50 = Math.floor(resto / 50);
-	const resto = valorSaque % 50;
+	resto = resto % 50;
 
 	const cedula_10 = Math.floor(resto / 10);
 
 	// Exibir as notas apenas se houver.
 	if (cedula_100 > 0) {
 		outNotas100.textContent = "Notas de R$100: " + cedula_100;
+		outNotas100.style.color = "cyan";
 	}
 
 	if (cedula_50 > 0) {
 		outNotas50.textContent = "Notas de R$50: " + cedula_50;
+		outNotas50.style.color = "cyan";
+
 	}
 
 	if (cedula_10 > 0) {
-		outNotas10.textContent = "Notas de R$10: " + cedula_10;
+		outNotas10.textContent = "NOtas de R$10: " + cedula_10;
+		outNotas10.style.color = "cyan";
 	}
+
 }
 
 const btnExibirNotasSaque = document.getElementById("btnExibirNotasSaque");
 btnExibirNotasSaque.addEventListener("click", exibirValorSaques);
+
 
 
 /*
